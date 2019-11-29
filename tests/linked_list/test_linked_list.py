654bce4e6aebe4ce5bb7e02bf43cadf7b7bb9bce
larrykooper <larry.kooper@gmail.com>
2019-11-28 20:56:23 -0500

    wrote some tests

--- tests/linked_list/test_linked_list.py
+++ tests/linked_list/test_linked_list.py
@@ -1,3 +1,5 @@
+import sys
+print(sys.path)
 from linked_list import LinkedList
 
 class TestLinkedList:
@@ -6,3 +8,10 @@ class TestLinkedList:
         l = LinkedList()
         l.insert_at_beginning(10)
         assert l.count() == 1
+
+
+    def test_remove_from_beginning(self):
+        l = LinkedList()
+        l.insert_at_beginning(20)
+        value = l.remove_from_beginning()
+        assert value == 20
